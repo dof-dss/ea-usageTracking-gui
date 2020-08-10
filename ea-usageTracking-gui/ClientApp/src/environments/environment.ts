1283e00766de96102d87c6d5e5c9f0bc2fd07d0a
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-08-10 15:26:56 +0100

    Tidy up settings

--- ea-usageTracking-gui/ClientApp/src/environments/environment.ts
+++ ea-usageTracking-gui/ClientApp/src/environments/environment.ts
@@ -6,12 +6,8 @@ export const environment = {
   production: false,
   authUri: 'https://ea-beta.auth.eu-west-2.amazoncognito.com',
   redirectUri: 'https://localhost:4200/',
-  //redirectUri: 'https://ea-usagetracking-gui.london.cloudapps.digital/',
   clientId: '3j0feko5ai0b58d9k8ecdjupga',
-  //apiUri: 'https://ea-usage-tracking.london.cloudapps.digital/api/'
-  //apiUri: 'https://83lhtxt45d.execute-api.eu-west-2.amazonaws.com/beta/api/'
- apiUri: 'https://localhost:44343/api/'
-  //apiUri: 'https://q3c58w7tu3.execute-api.eu-west-2.amazonaws.com/beta/api/v1/'
+  apiUri: 'https://localhost:44343/api/'
 };
 
 /*
