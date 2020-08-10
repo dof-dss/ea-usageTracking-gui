c45b758e076cd2ad25566f7f25517e2dcb74e58d
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-08-10 11:30:33 +0100

    Updarte CI

--- ea-usageTracking-gui/ClientApp/src/environments/environment.sandbox.ts
+++ ea-usageTracking-gui/ClientApp/src/environments/environment.sandbox.ts
@@ -1,7 +1,1 @@
-export const environment = {
-    production: false,
-    authUri: 'https://ea-beta.auth.eu-west-2.amazoncognito.com',
-    redirectUri: 'https://ea-usagetracking-gui-sandbox.london.cloudapps.digital/',
-    clientId: '3j0feko5ai0b58d9k8ecdjupga',
-    apiUri: 'https://ea-usage-tracking-sandbox.london.cloudapps.digital/api/v1/'
-  };
+export const environment = { production: false, authUri: 'https://ea-beta.auth.eu-west-2.amazoncognito.com', redirectUri: 'https://ea-usagetracking-gui-sandbox.london.cloudapps.digital/', clientId: '3j0feko5ai0b58d9k8ecdjupga', apiUri: 'https://ea-usage-tracking-sandbox.london.cloudapps.digital/api/v1/' };
