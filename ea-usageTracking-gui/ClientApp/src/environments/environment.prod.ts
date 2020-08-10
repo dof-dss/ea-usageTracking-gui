9cbcb5aceb8c388f1d555feda2a19a1be8ac8d9b
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-08-10 14:38:18 +0100

    Remove credentials

--- ea-usageTracking-gui/ClientApp/src/environments/environment.prod.ts
+++ ea-usageTracking-gui/ClientApp/src/environments/environment.prod.ts
@@ -1,7 +1,1 @@
-export const environment = {
-  production: true,
-  authUri: 'https://ea-prod.auth.eu-west-2.amazoncognito.com',
-  redirectUri: 'https://ea-usagetracking-gui.london.cloudapps.digital/',
-  clientId: '4qm8a133aj6ajvatq6fl9cfv53',
-  apiUri: 'https://usage-tracking.api.digitalni.gov.uk/v1/'
-};
+
--- ea-usageTracking-gui/ClientApp/src/environments/environment.staging.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    authUri: 'https://ea-staging.auth.eu-west-2.amazoncognito.com',
-    redirectUri: 'https://ea-usagetracking-gui-staging.london.cloudapps.digital/',
-    clientId: '5qd896gsoi3ndfshukh07t3s9o',
-    apiUri: 'https://usage-tracking-test.api.digitalni.gov.uk/v1/'
-  };
