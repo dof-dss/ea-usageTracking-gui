64948c21243c09b2ed45475af33a4066ace5a1e3
GrahamMcVea-dof <55088706+GrahamMcVea-dof@users.noreply.github.com>
2020-08-10 11:18:16 +0100

    Update circleCI

--- ea-usageTracking-gui/ClientApp/src/environments/environment.sandbox.ts
+++ ea-usageTracking-gui/ClientApp/src/environments/environment.sandbox.ts
@@ -3,6 +3,5 @@ export const environment = {
     authUri: 'https://ea-beta.auth.eu-west-2.amazoncognito.com',
     redirectUri: 'https://ea-usagetracking-gui-sandbox.london.cloudapps.digital/',
     clientId: '3j0feko5ai0b58d9k8ecdjupga',
-    //apiUri: 'https://ea-usage-tracking.london.cloudapps.digital/api/'
     apiUri: 'https://ea-usage-tracking-sandbox.london.cloudapps.digital/api/v1/'
   };
